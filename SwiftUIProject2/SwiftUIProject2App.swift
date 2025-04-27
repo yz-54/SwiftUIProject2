04beacf87e85ef64640ec7304ceec241b80086a5
yzhang <zhangyehua1996@gmail.com>
2025-04-27 14:23:57 -0500

    Initial Commit

---
+++ SwiftUIProject2/SwiftUIProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject2App.swift
+//  SwiftUIProject2
+//
+//  Created by Yehua Zhang on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
