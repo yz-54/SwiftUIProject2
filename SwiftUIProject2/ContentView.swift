73d3d282a3f7b23a0237828599bbc3882c3e3a40
yzhang <zhangyehua1996@gmail.com>
2025-04-27 14:28:30 -0500

    init push

--- SwiftUIProject2/ContentView.swift
+++ SwiftUIProject2/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var msg = "You are learnding swift."
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.theSimpsonsLearnding)
+            .resizable()
+                .scaledToFit()
+            Text(msg)
+                .font(.title3)
+                .fontWeight(.semibold)
+                .foregroundStyle(.orange)
+            // when button is pressed, the code inside the curly braces will excute
+            Button("Click Me!") {
+                msg = "Awesome!"
+            }.buttonStyle(.borderedProminent)
         }
         .padding()
+        
     }
 }
-
 #Preview {
     ContentView()
 }
